13d38e81e87699517fe7af350a717e0f8a7f75d6
mecmayn <78371584+mecmayn@users.noreply.github.com>
2021-03-06 21:40:01 -0600

    Update for New Item and Edit Item

--- groceryShoppingList/app/src/main/java/com/example/groceryshoppinglist/addNewItem.java
+++ groceryShoppingList/app/src/main/java/com/example/groceryshoppinglist/addNewItem.java
@@ -3,6 +3,7 @@ package com.example.groceryshoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class addNewItem extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class addNewItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
+        String[] choiceNames={"Categories","Produce","Dairy","Dry Goods","Meat","Frozen", "Bakery"};
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,choiceNames);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     }
 }
