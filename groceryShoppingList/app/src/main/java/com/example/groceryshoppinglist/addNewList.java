c711eb688975f31162d909881132c1d5ae2eabe5
Sebastian Braun <s.braun@dal.de>
2021-03-17 16:42:56 +0100

    add Invitation toast

--- groceryShoppingList/app/src/main/java/com/example/groceryshoppinglist/addNewList.java
+++ groceryShoppingList/app/src/main/java/com/example/groceryshoppinglist/addNewList.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -27,6 +28,11 @@ public class addNewList extends AppCompatActivity {
         Intent create = new Intent(this, Overview.class);
 
         startActivity(create);
+    }
+
+    public void sendInvitation(View view){
+        Toast.makeText(this, "Invitation has been sent.", Toast.LENGTH_SHORT).show();
+        return;
     }
 
     @Override
